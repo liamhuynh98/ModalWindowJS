e07e6f9f42d5cb6c2fe7d74018f44690f80e35a8
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-04 19:38:23 -0500

    Learned querySelectorAll scenario

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
-'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+//use querySelectorAll to select all the element with the same class name
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}
